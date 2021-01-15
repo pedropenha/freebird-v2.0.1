c3fe4a8f618e7e53948a55cd02163437d468b1e9
Pedro Penha <pedro.penha.martins@gmail.com>
2021-01-14 22:49:04 -0300

    initial commit

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject canvasPause;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                canvasPause.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                canvasPause.SetActive(false);
+            }
+        }
+    }
+}
